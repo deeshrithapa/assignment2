e3d9ea820bbff57aa4f73f5594a27827da34c174
deeshrithapa <deeshri.t@gmail.com>
2023-11-29 20:11:03 +0545

    . init

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: loginpage(),
+      home: SettingPage(),
     );
   }
 }
--- lib/pages/slides/SettingPage.dart
+++ lib/pages/slides/SettingPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class SettingPage extends StatefulWidget {
   SettingPage({Key? key}) : super(key: key);
@@ -7,35 +8,74 @@ class SettingPage extends StatefulWidget {
 }
 class _SettingPageState extends State<SettingPage>{
 
+  int _selectedIndex=0;
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.brown.shade50,
-        body: SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Container(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-    Padding(padding: EdgeInsets.only(top: 30.0)),
-    Text(
-    "Account Setting",
-    style: TextStyle(
-    fontSize: 20,
-    fontStyle: FontStyle.italic,
-    fontWeight: FontWeight.bold,
-    ),
-    ),
-          ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(padding: EdgeInsets.only(top: 30.0)),
+              Text(
+                "Account Setting",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+
         ),
 
-    ),
 
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric( horizontal: 15, vertical:20),
+          child: GNav(
 
-    ),
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey.shade800,
+            gap: 8,
+            onTabChange: (index){
+              setState(() {
+                _selectedIndex = index;
+              });
+
+            },
+
+            padding: EdgeInsets.all(16),
+            tabs: const[
+
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+              ),
+              GButton(
+                icon: Icons.settings,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: 2,
+          ),
+        ),
+      ),
     );
 
-
-}
+  }
 }
