55db0aa509f00840964886046a8ac083bee0332b
praasam <maharjanprasamsha102@gmail.com>
2023-12-19 13:12:56 +0545

    signup validation

--- lib/pages/authentication/signup.dart
+++ lib/pages/authentication/signup.dart
@@ -6,114 +6,150 @@ import 'package:skin_sync/pages/authentication/login.dart';
 import 'package:skin_sync/pages/slides/Dashboard.dart';
 
 class signuppage extends StatefulWidget {
-  const signuppage({super.key});
+  const signuppage({Key? key}) : super(key: key);
 
   @override
   State<signuppage> createState() => _signuppageState();
 }
 
 class _signuppageState extends State<signuppage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    String email = "", pass="";
     return Scaffold(
       backgroundColor: Colors.brown.shade50,
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.only(top: 20),
           child: Column(
-              children: [
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("lib/images/skinsync.png", height: 200, width: 150,),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("SkinSync",
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("lib/images/skinsync.png", height: 200, width: 150,),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("SkinSync",
                       style: GoogleFonts.aladin(fontSize: 40, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold,)
-                     ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Register Here",
-                        style: GoogleFonts.aladin(fontSize: 25, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold,)
-                    ),],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Register Here",
+                      style: GoogleFonts.aladin(fontSize: 25, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold,)
+                  ),
+                ],
+              ),
 
-                Container(
+              Form(
+                key: _formKey,
+                child: Container(
                   padding: EdgeInsets.only(right: 35, left: 35),
                   child: Column(
                     children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: "Full Name",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 5,),
-                      TextField(
-                        decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: "Address",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 5,),
-                      TextField(
-                        decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: "Mobile Number",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            )
-                        ),
-                      ),
-                      SizedBox(height: 5,),
-                      TextField(
-                        onChanged: (value){
-                          email = value;
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your full name';
+                          }
+                          return null;
                         },
                         decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: "Email",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            )
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Full Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
                       SizedBox(height: 5,),
-                      TextField(
-                        onChanged: (value){
-                          pass = value;
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your address';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Address",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your mobile number';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Mobile Number",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter an email address';
+                          }
+                          // You can add more complex email validation if needed
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 5,),
+                      TextFormField(
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a password';
+                          }
+                          // You can add more complex password validation if needed
+                          return null;
                         },
                         obscureText: true,
                         decoration: InputDecoration(
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            hintText: "Password",
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)
-                            )
+                          fillColor: Colors.grey.shade100,
+                          filled: true,
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
                       ),
 
-                      SizedBox(height: 20,),
-                      /*Container(
+                      SizedBox(height: 20),
+
+                      Container(
                         height: 50,
                         width: 150,
                         decoration: BoxDecoration(
@@ -121,98 +157,77 @@ class _signuppageState extends State<signuppage> {
                           borderRadius: BorderRadius.circular(30),
                         ),
                         child: ElevatedButton(
-                          onPressed: () async {
-                            try {
-                              final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                                email: email,
-                                password: pass,
-                              );
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'weak-password') {
-                                print('The password provided is too weak.');
-                              } else if (e.code == 'email-already-in-use') {
-                                print('The account already exists for that email.');
-                              }
-                            } catch (e) {
-                              print(e);
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              // Validation passed, attempt to create an account
+                              signUpWithEmailAndPassword();
                             }
-
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => dashpage()),
-                            );
-                          },
-                          child: Center(
-                            // Center the Text widget within the GestureDetector
-                            child: Text(
-                              "Sign In",
-                              style: GoogleFonts.aladin(color: Colors.white, fontSize: 20),
-                            ),
-                          ),
-                        ),
-                      ),*/
-
-                      Container(
-                        height: 50,width: 150,
-                        decoration: BoxDecoration(color: Colors.brown.shade200,
-                            borderRadius: BorderRadius.circular(30)),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            try {
-                              final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                                email: email,
-                                password: pass,
-                              );
-                            } on FirebaseAuthException catch (e) {
-                              if (e.code == 'weak-password') {
-                                print('The password provided is too weak.');
-                              } else if (e.code == 'email-already-in-use') {
-                                print('The account already exists for that email.');
-                              }
-                            } catch (e) {
-                              print(e);
-                            }
-                            //Navigate
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=> loginpage()),);
                           },
                           style: ElevatedButton.styleFrom(
-                            primary: Colors.brown.shade200, // Set the background color of the button
+                            primary: Colors.brown.shade200,
                           ),
-                          child: Text("Sign In", style: GoogleFonts.aladin(color: Colors.white, fontSize: 30)),
+                          child: Text("Sign Up", style: GoogleFonts.aladin(color: Colors.white, fontSize: 30)),
                         ),
                       ),
 
                       SizedBox(height: 5,),
+
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                            Text("Or Sign in with",
-                              style: TextStyle(
-                                  fontSize: 14,),),
+                          Text("Or Sign in with",
+                            style: TextStyle(
+                              fontSize: 14,
+                            ),
+                          ),
                         ],
                       ),
-            GestureDetector(
-              onTap: () {
-                signInWithGoogle(context);
-              },
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal, // Added to allow horizontal scrolling if needed
-                padding: const EdgeInsets.only(left: 40),
-                child: Row(
-                  children: [
-                    Image.asset("lib/images/google.png", height: 50, width: 30,),
-                    SizedBox(width: 35),
-                  ],
+
+                      GestureDetector(
+                        onTap: () {
+                          signInWithGoogle(context);
+                        },
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal, // Added to allow horizontal scrolling if needed
+                          padding: const EdgeInsets.only(left: 40),
+                          child: Row(
+                            children: [
+                              Image.asset("lib/images/google.png", height: 50, width: 30,),
+                              SizedBox(width: 35),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-],
-        ),
-                  ),
-                ],
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void signUpWithEmailAndPassword() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      // Navigate to the dashboard or perform other actions
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => dashpage()),
+      );
+    } on FirebaseAuthException catch (e) {
+      // Handle specific FirebaseAuth exceptions
+      print('FirebaseAuthException: ${e.message}');
+      // Show user-friendly error message (SnackBar or Dialog)
+    } catch (e) {
+      print(e);
+      // Show a generic error message (SnackBar or Dialog)
+    }
   }
 }
