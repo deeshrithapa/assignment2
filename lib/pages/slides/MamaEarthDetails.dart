070905030fef7d156b61d708a96f9856256667df
praasam <maharjanprasamsha102@gmail.com>
2024-01-08 17:00:06 +0545

    final changes

--- lib/pages/slides/MamaEarthDetails.dart
+++ lib/pages/slides/MamaEarthDetails.dart
@@ -1,13 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:provider/provider.dart';
 
-
-
-import '../../images/data_detailpage.dart';
 import '../../models/Cart.dart';
 import '../../models/cart_provider.dart';
 import 'CartPage.dart';
@@ -21,8 +17,8 @@ class MamaEarthDetails extends StatefulWidget {
 }
 
 class _DetailPageState extends State<MamaEarthDetails> {
-
   List<Map<String, dynamic>> mamaProducts = [];
+
   // Fetch data from Firestore
   Future<void> fetchDataFromFirestore() async {
     try {
@@ -47,13 +43,8 @@ class _DetailPageState extends State<MamaEarthDetails> {
     super.initState();
     fetchDataFromFirestore();
   }
+
   int _selectedIndex = 0;
-
-  // Add a controller for the search field
-  TextEditingController searchController = TextEditingController();
-
-  // List to hold the search results
-  List<Map<String, dynamic>> searchResults = [];
 
   void _onTabChange(int index) {
     setState(() {
@@ -109,85 +100,94 @@ class _DetailPageState extends State<MamaEarthDetails> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'SkinSync',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
+                      "SkinSync",
+                      style: GoogleFonts.aladin(
+                        fontSize: 30,
+                        fontStyle: FontStyle.normal,
                       ),
                     ),
                     SizedBox(height: 8),
                     Text(
-                      'MamaEarth Products',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
+                      "MamaEarth Products",
+                      style: GoogleFonts.aladin(
+                        fontSize: 25,
+                        fontStyle: FontStyle.normal,
                       ),
                     ),
                   ],
                 ),
               ),
+              SizedBox(height: 20),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 20,
+                      childAspectRatio: 0.73,
+                    ),
+                    itemCount: mamaProducts.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
 
-              // Add Search Bar
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: searchController,
-                  decoration: InputDecoration(
-                    hintText: 'Search MamaEarth Products',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: () {
-                        searchController.clear();
-                        setState(() {
-                          searchResults.clear();
-                        });
-                      },
-                    ),
+                            Expanded(
+                              child: Container(
+                                width: double.maxFinite,
+                                decoration: BoxDecoration(
+                                  color: Color(mamaProducts[index]['color'] ?? 0xFFFFFFFF),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Image.network(
+                                    mamaProducts[index]['img'],
+                                    height: 160,
+                                    fit: BoxFit.cover, // This will ensure the image covers the entire area
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            Text(
+                              mamaProducts[index]['name'] ?? '',
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                            ),
+
+                            Text(
+                              'Rs.' + (mamaProducts[index]['price'] ?? 0).toString(),
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                            ),
+
+
+                            ElevatedButton(
+                              onPressed: () {
+                                var cartProvider = context.read<CartProvider>();
+                                cartProvider.addToCart(CartItem(
+                                  name: mamaProducts[index]['name'],
+                                  price: double.parse(mamaProducts[index]['price'].toString()),
+                                  imagePath: mamaProducts[index]['img'],
+                                ));
+
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Added to Cart: ${mamaProducts[index]['name']}'),
+                                  ),
+                                );
+                              },
+                              child: Text('Add to Cart'),
+                            ),
+
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
               ),
-
-              // Display Search Results
-              if (searchResults.isNotEmpty)
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 20,
-                        childAspectRatio: 0.73,
-                      ),
-                      itemCount: searchResults.length,
-                      itemBuilder: (context, index) {
-                        return buildProductCard(searchResults[index]);
-                      },
-                    ),
-                  ),
-                )
-              else
-              // Display MamaEarth Products
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.fromLTRB(25, 0, 0, 0),
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 10,
-                        crossAxisSpacing: 20,
-                        childAspectRatio: 0.73,
-                      ),
-                      itemCount: 6,
-                      itemBuilder: (context, index) {
-                        return buildProductCard(data_detailpage[index + 10]);
-                      },
-                    ),
-                  ),
-                ),
             ],
           ),
         ),
@@ -227,80 +227,6 @@ class _DetailPageState extends State<MamaEarthDetails> {
             selectedIndex: 0,
           ),
         ),
-      ),
-    );
-  }
-
-  // Extracted widget to build a product card
-  Widget buildProductCard(Map<String, dynamic> product) {
-    return GestureDetector(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                color: Color(product['color']),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Center(
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 55,
-                        backgroundColor: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 55,
-                        backgroundColor: Color(product['color']).withOpacity(0.5),
-                      ),
-                    ),
-                    Image.asset(
-                      product['image'],
-                      height: 160,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Text(
-            product['name'],
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-          ),
-          Text(
-            r'Rs.' + product['price'],
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-          ),
-          // Add to Cart Button
-          ElevatedButton(
-            onPressed: () {
-              // Get the CartProvider instance
-              var cartProvider = context.read<CartProvider>();
-
-              // Add the selected item to the cart
-              cartProvider.addToCart(CartItem(
-                name: product['name'],
-                price: double.parse(product['price']),
-                imagePath: product['image'],
-              ),);
-
-              // Show a snackbar or navigate to the cart page
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Added to Cart: ${product['name']}'),
-                ),
-              );
-            },
-            child: Text('Add to Cart'),
-          ),
-        ],
       ),
     );
   }
